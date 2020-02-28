b084d1f9c040f4ac0cd5016e00213e43006e66a5
Andrey <andrey98vas@gmail.com>
2020-02-28 04:53:52 +0300

    -Renamed directory -Increased max length text fields in table 'item'

--- api/news/models.py
+++ api/news/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 
 class Item(models.Model):
-    caption = models.CharField(verbose_name='Caption', db_index=True, max_length=64)
+    caption = models.CharField(verbose_name='Caption', db_index=True, max_length=256)
     date = models.DateField(verbose_name='Date')
-    source = models.CharField(verbose_name='Source', max_length=128)
+    source = models.CharField(verbose_name='Source', max_length=512)
     #user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
