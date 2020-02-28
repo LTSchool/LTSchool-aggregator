f046468988eb7a9850cde0ec720c8f2f6cba2d24
Furkas91 <38116222+Furkas91@users.noreply.github.com>
2020-02-28 21:48:10 +0300

    Update Rss.py
    Editing the script for more convenient operation

--- Grabber/Rss.py
+++ Grabber/Rss.py
@@ -1,4 +1,11 @@
 import feedparser
-d = feedparser.parse('https://www.liga.net/news/all/rss.xml')
-for post in d.entries:
-    print(post.title+ "\n" + post.description + "\n" + post.link + "\n")
+
+
+def rssgrabber(link):
+    return feedparser.parse(link)
+
+
+if __name__ == '__main__':
+    d = rssgrabber('https://www.liga.net/news/all/rss.xml')
+    for post in d.entries:
+        print(post.title+ "\n" + post.description + "\n" + post.link + "\n")
