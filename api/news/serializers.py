00433e9c430ad77c499d9490dbb5309f8bbc9d3f
Andrey <andrey98vas@gmail.com>
2020-03-02 22:46:50 +0300

    -added authentication -added registration -updated .gitignore, now is the very cool thing

--- api/news/serializers.py
+++ api/news/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from news.models import Item
+from news.models import Item, User
 
 
 class ItemDetailSerializer(serializers.ModelSerializer):
@@ -14,3 +14,22 @@ class ItemListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['caption', 'date', 'source']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
