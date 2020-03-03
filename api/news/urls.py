2e71e9464f57c28e5efc5bad37476963ea522fa5
Vladimir Toropov <toropov29@rambler.ru>
2020-03-03 18:01:47 +0300

    added API functions
    Added viewing, deleting and modification of record of db

--- api/news/urls.py
+++ api/news/urls.py
@@ -6,5 +6,6 @@ from news.views import *
 app_name = 'news'
 urlpatterns = [
     path('create/', CreateItemView.as_view()),
-    path('all/', ListItemView.as_view())
+    path('all/', ListItemView.as_view()),
+    path('id/<int:pk>/', RetrieveUpdateDestroyItemView.as_view(), name='Item')
 ]
